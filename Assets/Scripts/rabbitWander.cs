e07e274991f2c25cd771fbf4b91220d5041a820b
Jc-42 <83050748+Jc-42@users.noreply.github.com>
2021-04-22 23:51:54 -0500

    Added NavMesh

--- Assets/Scripts/rabbitWander.cs
+++ Assets/Scripts/rabbitWander.cs
@@ -1,21 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class rabbitWander : MonoBehaviour
 {
     public int rabbitHealth = 14;
     public int rabbitHunger = 0;
     public int rabbitThirst = 0;
-
+    public static Vector3 test;
     void Start()
     {
-        
+        InvokeRepeating("RandomNavSphere", 10f, 5); 
     }
 
     
     void Update()
     {
+      //  Debug.Log(test);
+    }
+
+
+
+    public static Vector3 RandomNavSphere (Vector3 origin, float distance, int layermask) {
+
+        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * distance;
+
+        randomDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randomDirection, out navHit, distance, layermask);
+        
+        test = navHit.position;
+        
+        return navHit.position;
+
         
     }
+
+
+    
 }
