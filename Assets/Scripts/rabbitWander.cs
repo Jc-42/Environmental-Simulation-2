9709491d0d591385c01582bd663ceacbe9083a49
Jc-42 <83050748+Jc-42@users.noreply.github.com>
2021-04-22 22:31:07 -0500

    Added Existing Project

---
+++ Assets/Scripts/rabbitWander.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rabbitWander : MonoBehaviour
+{
+    public int rabbitHealth = 14;
+    public int rabbitHunger = 0;
+    public int rabbitThirst = 0;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
